9f6fe0430ffa05a8d2ce365031b726483c2600e1
NanthiniP1 <NanthiniP1@hexaware.com>
2021-02-19 16:06:27 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Test{
+int x=10;
+//static int y=10;
+public Test(int a){
+a=x;
+}
+//public static void demo(){
+//System.out.println(x);
+//System.out.println(y);
+//}
+}
+
+class Main{
+public static void main(String args[]){
+Test t=new Test(5);
+System.out.println(t.);
+}
+}
